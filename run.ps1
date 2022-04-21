99e1acc8b5857bdcc5917e8894c6d0e5b505dc4e
Aidan Brubeck <50123545+Ayydeeen@users.noreply.github.com>
2022-04-21 15:00:01 -0400

    Create run.ps1

---
+++ run.ps1
@@ -0,0 +1,10 @@
+From AD:
+
+Get-aduser -Filter 'enabled -eq $true' | select Name | sort Name | Export-Csv -Path C:\Users.csv
+ 
+
+From Local Exchange Server Management Console:
+
+$Users = Get-Content "C:\Users\Xeroxitadmin\Desktop\Users.csv"
+
+ForEach ($user in $Users) { Remove-UserPhoto $user -confirm:$false }
